f08d0d0bf9a9871b84a2779d4895ca4f3c1bfca9
jonolds <jonolds@gmail.com>
2019-02-27 20:12:47 -0600

    win

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,16 +1,11 @@
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.File;
 import java.io.IOException;
 import java.util.StringTokenizer;
 import java.util.stream.StreamSupport;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -21,7 +16,6 @@ import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-@SuppressWarnings("unused")
 public class WordCount {
 	static final String DELIM = "*-*-*";
 	
@@ -55,7 +49,6 @@ public class WordCount {
 				for (IntWritable val : values)
 					sum += Integer.parseInt(val.toString());
 				result.set(sum);
-
 				context.write(key, result);
 			}
 		}
@@ -65,11 +58,17 @@ public class WordCount {
 	
 	public static void main(String[] args) throws Exception {
 		Job job = init(args);
+		job.setJarByClass(WordCount.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
 		
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		MultipleOutputs.addNamedOutput(job, "mapCallCount", TextOutputFormat.class, Text.class, IntWritable.class);
-		
+
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 	
@@ -80,29 +79,7 @@ public class WordCount {
 			System.err.println("Usage: wordcount <in> <out>");
 			System.exit(2);
 		}
-		FileUtils.deleteDirectory(new File(otherArgs[1]));
-		Job job = Job.getInstance(conf, "word count");
-		job.setJarByClass(WordCount.class);
-		job.setMapperClass(TokenizerMapper.class);
-		job.setCombinerClass(IntSumReducer.class);
-		job.setReducerClass(IntSumReducer.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
-		return job;
-	}
-}
-
-class BigramWritable implements WritableComparable<BigramWritable> {
-	Text a, b;
-	BigramWritable() { this.a = null; this.b = null; }
-	BigramWritable(String a, String b) { this.a = new Text(a); this.b = new Text(b); }
-	
-	public void write(DataOutput out) throws IOException {
-	}
-
-	public void readFields(DataInput in) throws IOException {		
-	}
-	public int compareTo(BigramWritable o) {
-		return 0;
+		new Path(args[1]).getFileSystem(conf).delete(new Path(otherArgs[1]), true);
+		return Job.getInstance(conf, "word count");
 	}
 }
